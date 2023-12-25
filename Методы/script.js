d5f335c3619c07b7211ab977955054867e32ee1c
Nurbolat <nurik7189@gmail.com>
2023-12-25 16:12:28 +0600

    методы

---
+++ Методы/script.js
@@ -0,0 +1,22 @@
+'use sctrict';
+
+const str = 'test';
+
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('f'));
+
+const logg = 'Hello world';
+console.log(logg.slice(6, 11));
+console.log(logg.substring(4, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
